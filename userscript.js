2aa418c2be32436349273552dcb93d101f2984c9
TonyD365 <tonydong365@outlook.com>
2025-12-01 09:16:07 -0500

    Add throttle control function to userscript

--- userscript.js
+++ userscript.js
@@ -7,3 +7,21 @@
 // @match        *://geo-fs.com/geofs.php?*
 // @grant        none
 // ==/UserScript==
+
+(async function(){
+  'use strict';
+  function setThrottle(value){
+    if (geofs.aircraft.instance.engine.throttle > value){
+      for (let i = geofs.aircraft.instance.engine.throttle;i < value;i = geofs.aircraft.instance.engine.throttle){
+        geofs.aircraft.instance.engine.throttle -= 0.05
+      }
+      geofs.aircraft.instance.engine.throttle = value
+    }else if (geofs.aircraft.instance.engine.throttle < value){
+      for (let i = geofs.aircraft.instance.engine.throttle;i > value;i = geofs.aircraft.instance.engine.throttle){
+        geofs.aircraft.instance.engine.throttle += 0.05;
+      }
+      geofs.aircraft.instance.engine.throttle = value
+    }
+  }
+  
+}){}
