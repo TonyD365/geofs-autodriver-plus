c76005f84ef877290bf999b6c3677e10d0b59f0f
TonyD365 <tonydong365@outlook.com>
2025-12-01 16:17:12 -0500

    Update download and update URLs in userscript.js

--- userscript.js
+++ userscript.js
@@ -8,8 +8,8 @@
 // @match        https://*.geo-fs.com/geofs.php*
 // @grant        none
 // @run-at       document-idle
-// @downloadURL  https://raw.githubusercontent.com/TonyD365/geofs-autodriver-plus/refs/heads/main/userscript.js
-// @updateURL    https://raw.githubusercontent.com/TonyD365/geofs-autodriver-plus/refs/heads/main/userscript.js
+// @downloadURL  https://tonyd365.github.io/geofs-autodriver-plus/userscript.js
+// @updateURL    https://tonyd365.github.io/geofs-autodriver-plus/userscript.js
 // ==/UserScript==
 
 (function(){
