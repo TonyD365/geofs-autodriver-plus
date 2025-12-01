f44a39e07a357984d9f6079bb7636709e9fe3f67
TonyD365 <tonydong365@outlook.com>
2025-12-01 16:08:48 -0500

    Fix throttle adjustment logic in setThrottle function

--- userscript.js
+++ userscript.js
@@ -30,7 +30,7 @@
   async function setThrottle(value){
     if (geofs.aircraft.instance.engine.throttle > value){
       for (let i = geofs.aircraft.instance.engine.throttle;i > value;i = geofs.aircraft.instance.engine.throttle){
-        geofs.aircraft.instance.engine.throttle -= 0.05
+        controlers -= 0.05
       }
       geofs.aircraft.instance.engine.throttle = value
     }else if (geofs.aircraft.instance.engine.throttle < value){
