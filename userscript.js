cb4f06ddbb4d7124c8790bf50fe098456a96603d
TonyD365 <tonydong365@outlook.com>
2025-12-01 08:48:54 -0500

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,1 +1,9 @@
-
+// ==UserScript==
+// @name         geofs autodriver++
+// @namespace    https://
+// @version      1.0
+// @description  在网页右下角添加返回顶部按钮
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
